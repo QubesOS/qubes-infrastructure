QubesOS/qubes-infrastructure

commit d6ddc64a10998fc0e285fec092f7ec827bda5b37

Safely checkout qubes-builder
Verifying that a git commit has a valid tag pointing to it is complex.
Instead, just checkout a specific SHA and let `make get-sources` do the
heavy lifting.

build-infra/build-vm.sls
@@ -206,29 +206,22 @@ github.com:
 {% endif %}
 
 {% for builder in builders_list %}
-{% set config_baseurl = salt['pillar.get']('build-infra:build-envs:' + env + ':builders-list:' + builder + ':config:repository:baseurl', 'https://github.com/QubesOS/qubes-') %}
+{% set config_baseurl = salt['pillar.get']('build-infra:build-envs:' + env + ':builders-list:' + builder + ':config:repository:baseurl', 'ssh://github.com/QubesOS/qubes-') %}
 {% set config_repo = salt['pillar.get']('build-infra:build-envs:' + env + ':builders-list:' + builder + ':config:repository:component', 'release-configs') %}
 {% set config_file = salt['pillar.get']('build-infra:build-envs:' + env + ':builders-list:' + builder + ':config:file') %}
 {% set keys =  salt['pillar.get']('build-infra:build-envs:' + env + ':builders-list:' + builder + ':keys', []) %}
 
-{{builder}}-get:
-  git.latest:
-    - name : https://github.com/QubesOS/qubes-builder
-    - target: {{ builder }}
-    - user: user
-
 {{builder}}-check:
-  cmd.run:
-    - name: git verify-tag --raw "$(git describe)" 2>&1 >/dev/null | grep '^\[GNUPG:\] TRUST_FULLY'
-    - cwd: {{ builder }}
+  cmd.script:
+    - source: salt://build-infra/safe-checkout-sha
+    - args: ['--', 'ssh://git@github.com/QubesOS/qubes-builder', {{ builder|yaml_encode }}, 'e8293887c39a5e39fe70d2e96564681a37ef8248']
     - runas: user
     - require:
-      - git: {{builder}}-get
       - gpg: {{qubes_master_key_fpr}}
 
 {{builder}}-init:
   cmd.run:
-    - name: "BUILDERCONF= GIT_URL_builder=https://github.com/QubesOS/qubes-builder COMPONENTS=builder make get-sources"
+    - name: "BUILDERCONF= GIT_URL_builder=ssh://github.com/QubesOS/qubes-builder COMPONENTS=builder make get-sources"
     - cwd: {{ builder }}
     - runas: user
     - require:
@@ -242,6 +235,7 @@ github.com:
     - runas: user
     - require:
       - cmd: {{builder}}-init
+      - cmd: {{builder}}-check
 {% endfor %}
 
 {{builder}}-configs:

build-infra/safe-checkout-sha
@@ -1,0 +1,22 @@
+#!/bin/bash --
+{
+set -eufo pipefail
+export LC_ALL=C
+usage () {
+    printf %s\\n "USAGE: $0 -- REMOTE DIRECTORY SHA" >&2
+    exit 1
+}
+if [[ "$#" -ne 4 ]] || [[ "x$1" != 'x--' ]]; then usage; fi
+remote=$2 directory=$3 sha=$4
+success=no
+trap '[ "$success" = yes ] || rm -rf -- "$directory"' EXIT
+[[ ${#sha} -eq 40 ]] || [[ ${#sha} -eq 64 ]] || usage
+[[ $sha =~ ^[0-9a-f]+$ ]] || usage
+if [[ -d "$directory" ]]; then
+    git clone --no-checkout -- "$remote" "$directory"
+fi
+git -C "$directory" checkout "$sha"
+git update-ref FETCH_HEAD "$sha"
+git reset --hard "$sha"
+success=yes
+}

commit d329ed23821dfda55757380487fafadc07e5de43

Fix calling safe-checkout-sha
The original version never really worked ('[--' was passed literally as
the first argument). cmd.script does not support arguments list, only
a single string with space-separated arguments.

Fixes: d6ddc64 Safely checkout qubes-builder

build-infra/build-vm.sls
@@ -216,7 +216,7 @@ github.com:
 {{builder}}-check:
   cmd.script:
     - source: salt://build-infra/safe-checkout-sha
-    - args: ['--', 'https://github.com/QubesOS/qubes-builder', {{ builder }}, 'dfc98842ff2184dc936d4f3febdad79a8ab08029']
+    - args: "-- https://github.com/QubesOS/qubes-builder {{ builder }} dfc98842ff2184dc936d4f3febdad79a8ab08029"
     - runas: user
     - creates: {{builder}}
 